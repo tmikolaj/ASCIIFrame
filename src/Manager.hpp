564a62e51aca831f9b7d8f61f66fb6983ac3b7cd
mikolaj <mikolajt55@gmail.com>
2025-06-13 19:49:32 +0200

    Remove clear, include iostream and Window.hpp

--- src/Manager.hpp
+++ src/Manager.hpp
@@ -1,12 +1,12 @@
 #ifndef MENU_HPP
 #define MENU_HPP
 
-#include <cstdlib>
+#include <iostream>
+#include "Window.hpp"
 
 class Manager {
 public:
     void start();
-    void clear();
 };
 
 #endif //MENU_HPP
