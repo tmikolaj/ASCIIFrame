d1c097c14e80261a9412dc4e408b07fad910f005
mikolaj <mikolajt55@gmail.com>
2025-06-13 19:50:26 +0200

    Add variables and methods

--- src/Window.hpp
+++ src/Window.hpp
@@ -1,9 +1,26 @@
 #ifndef WINDOW_HPP
 #define WINDOW_HPP
 
-#include <array>
+#include <vector>
+#ifdef _WIN32
+#include <conio.h>
+#else
+#include <ncurses.h>
+#endif
 
 class Window {
+private:
+    int m_x;
+    int m_y;
+    std::vector<std::vector<char>> frame;
+
+    void runNcurses();
+    void runConioh();
+    void display();
+    char changeCharacter();
+public:
+    Window(int x, int y);
+    ~Window() = default;
     void run();
 };
 
