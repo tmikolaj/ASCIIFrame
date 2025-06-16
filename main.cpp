fade9c23fa0c2057b12fb1c674be87bc47525f42
mikolaj <mikolajt55@gmail.com>
2025-06-16 19:57:55 +0200

    Add menu method in Manager which replaces most of the main method code

--- main.cpp
+++ main.cpp
@@ -1,30 +1,14 @@
 #include "src/Manager.hpp"
+#include "src/Window.hpp"
 
 int main() {
     Manager manager;
-    repeat: // GOTO from line 24
-    std::cout << "Welcome to ASCIIFrame!" << '\n' << '\n';
-    std::cout << "1. Start" << '\n';
-    std::cout << "2. Exit" << '\n';
-    char option;
-    std::cin >> option;
-
-    if (option == '1') {
-#ifdef _WIN32
-        system("cls");
-#else
-        system("clear");
-#endif
-        manager.start();
-    } else if (option == '2') {
-        exit(0);
+    bool cont = manager.menu();
+    if (cont) {
+        std::pair<int, int> dimensions = manager.start();
+        Window window(dimensions.first, dimensions.second, manager);
+        window.run();
     } else {
-        std::cout << "Invalid option! Type 1 or 2!" << '\n';
-#ifdef _WIN32
-        system("cls");
-#else
-        system("clear");
-#endif
-        goto repeat;
+        exit(0);
     }
 }
