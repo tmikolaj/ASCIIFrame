633c6932e644f68dc0b015f53e2d2036cea23c5b
mikolaj <mikolajt55@gmail.com>
2025-06-16 20:00:46 +0200

    Add frames for animation and playing the whole animation

--- src/Window.hpp
+++ src/Window.hpp
@@ -1,7 +1,7 @@
 #ifndef WINDOW_HPP
 #define WINDOW_HPP
 
-#include <vector>
+#include "Manager.hpp"
 #ifdef _WIN32
 #include <conio.h>
 #else
@@ -12,14 +12,19 @@ class Window {
 private:
     int m_x;
     int m_y;
-    std::vector<std::vector<char>> frame;
+
+    int currentFrame;
+    int framesCount;
+    std::vector<std::vector<std::vector<char>>> animation;
 
     void runNcurses();
     void runConioh();
-    void display();
+    void display(int currf);
     char changeCharacter();
+
+    Manager& m_mgr;
 public:
-    Window(int x, int y);
+    Window(int x, int y, Manager& mgr);
     ~Window() = default;
     void run();
 };
