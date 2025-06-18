86faa49197ac4f81cd84f33e702f04bf6da88fe8
mikolaj <mikolajt55@gmail.com>
2025-06-18 19:12:00 +0200

    Add load, save and overloaded menu function

--- src/Manager.hpp
+++ src/Manager.hpp
@@ -1,7 +1,9 @@
 #ifndef MENU_HPP
 #define MENU_HPP
 
+#include <cstdlib>
 #include <iostream>
+#include <fstream>
 #include <utility>
 #include <vector>
 
@@ -12,7 +14,10 @@ public:
     Manager();
     ~Manager() = default;
     std::pair<int, int> start();
-    bool menu();
+    void menu();
+    void menu(std::vector<std::vector<std::vector<char>>> &_a);
+    void save(std::string fileName, const std::vector<std::vector<std::vector<char>>> &a);
+    void load(std::string fileName, std::vector<std::vector<std::vector<char>>> &a);
     void playAnimation(const std::vector<std::vector<std::vector<char>>>& a);
 };
 
