dbfae7be86b765e7de65e0c4bea9d99ee38548cd
mikolaj <mikolajt55@gmail.com>
2025-06-18 19:11:24 +0200

    Delete variable and if statements

--- main.cpp
+++ main.cpp
@@ -3,12 +3,9 @@
 
 int main() {
     Manager manager;
-    bool cont = manager.menu();
-    if (cont) {
-        std::pair<int, int> dimensions = manager.start();
-        Window window(dimensions.first, dimensions.second, manager);
-        window.run();
-    } else {
-        exit(0);
-    }
+    manager.menu();
+
+    std::pair<int, int> dimensions = manager.start();
+    Window window(dimensions.first, dimensions.second, manager);
+    window.run();
 }
